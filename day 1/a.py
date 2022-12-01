b75bce31a19af627b4982c5f8aac0c9f2dfbe49f
nixnull <nichevans@protonmail.com>
2022-12-01 10:41:59 -0600

    removed unnecessary lambda

--- day 1/a.py
+++ day 1/a.py
@@ -1,9 +1,5 @@
 with open('input.txt', 'r') as infile:
-    elves = [x.split("\n") for x in infile.read()[:-1].split("\n\n")]
-    
-    to_nums = lambda x: [eval(y) for y in x]
-    
-    elves = [to_nums(x) for x in elves]
+    elves = [[int(c) for c in x.split("\n")] for x in infile.read()[:-1].split("\n\n")]
     
     print("Part A, Top Elf:")
     print(sorted(sum(x) for x in elves)[-1:])
