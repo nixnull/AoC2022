08b327db979e7a65522490015c71d5969d1edf14
nixnull <nichevans@protonmail.com>
2022-12-01 11:35:05 -0600

    print(f'') exists

--- day 1/answers.py
+++ day 1/answers.py
@@ -1,10 +1,6 @@
 with open('input.txt', 'r') as infile:
     elves = [[int(c) for c in x.split("\n")] for x in infile.read()[:-1].split("\n\n")]
     
-    print("Part A, Top Elf:")
-    print(max(sum(x) for x in elves))
+    print(f"Part A, Top Elf: {max(sum(x) for x in elves)}")
     
-    print()
-    
-    print("Part B, Top Three Elves Summed:")
-    print(sum(sorted([sum(x) for x in elves])[-3:]))
+    print(f"Part B, Top Three Elves Summed: {sum(sorted([sum(x) for x in elves])[-3:])}")
