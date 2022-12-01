19e77bfe6fbdbcd43306e79bee3ba22a28a60569
nixnull <nichevans@protonmail.com>
2022-12-01 10:43:06 -0600

    and streamlined a

--- day 1/a.py
+++ day 1/a.py
@@ -2,7 +2,7 @@ with open('input.txt', 'r') as infile:
     elves = [[int(c) for c in x.split("\n")] for x in infile.read()[:-1].split("\n\n")]
     
     print("Part A, Top Elf:")
-    print(sorted(sum(x) for x in elves)[-1:])
+    print(max(sum(x) for x in elves))
     
     print()
     
